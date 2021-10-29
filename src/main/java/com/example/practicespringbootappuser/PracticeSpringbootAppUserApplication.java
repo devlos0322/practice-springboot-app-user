236dc6c40ddbea6ebd980c5eeab88b730d18a708
devlos0322 <devlos0322@gmail.com>
2021-10-29 21:52:25 +0900

    Initial commit

---
+++ src/main/java/com/example/practicespringbootappuser/PracticeSpringbootAppUserApplication.java
@@ -0,0 +1,13 @@
+package com.example.practicespringbootappuser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PracticeSpringbootAppUserApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PracticeSpringbootAppUserApplication.class, args);
+    }
+
+}
